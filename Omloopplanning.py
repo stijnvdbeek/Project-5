1f0705f652e14b33ec9eb1d2f9d6a38d73f34c83
Bram Holla <bram.holla@example.com>
2024-09-19 14:34:49 +0200

    test met notebook

---
+++ Omloopplanning.py
@@ -0,0 +1,10 @@
+# Importeren van benodigde libraries
+import pandas as pd
+
+# Functie om data in te laden
+def load_data():
+    # Lees de Excel bestanden in DataFrames
+    df_omloopplanning = pd.read_excel('omloopplanning.xlsx', engine='openpyxl')
+    df_dienstregeling = pd.read_excel('Connexxion data - 2024-2025.xlsx', engine='openpyxl')
+    return df_omloopplanning, df_dienstregeling
+
